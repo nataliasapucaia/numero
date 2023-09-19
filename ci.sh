5e0615a2120f6a0c8cab9b28d23350e9d6ff6f18
Natália Sapucaia <natisapucaia@gmail.com>
2023-09-19 09:16:19 -0300

    update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'platform=iOS Simulator,name=iPhone 8' test
-xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'generic/platform=iOS' -configuration Release build 
-CODE_SIGNING_ALLOWED=NO
+xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'platform=iOS Simulator,name=iPhone 13' test
+xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'generic/platform=iOS' -configuration Release build CODE_SIGNING_ALLOWED=NO
+
 
